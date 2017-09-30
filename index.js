064bdd2a3a902e4ddc20dec9b7ac663bc72f9e7d
Deformator <dammandrii@gmail.com>
2017-09-29 20:00:36 -0400

    Final version 1) add logging req res 2) add .gitignore file to repo

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ var restify = require('restify')
 
     server.listen(PORT, HOST, function () {
         console.log('Server %s listening at %s', server.name, server.url)
-        console.log('Resources:')
-        console.log(' /products')
-        console.log(' /products/:id')  
+        console.log('Endpoints:')
+        console.log(server.url + '/products')
+        console.log(server.url + '/products/:id')  
       })
 
       server
@@ -30,11 +30,13 @@ var restify = require('restify')
     
     // Get all products in the system
     server.get('/products', function (req, res, next) {
+      console.log('>>>' + server.url + '/products: recieved GET request')
     
       // Find every entity within the given collection
       productsSave.find({}, function (error, products) {
     
         // Return all of the products in the system
+        console.log('<<<' + server.url + '/products: sending response')
         res.send(products)
         getCounter++;
         console.log('Processed request count --> sendGet: ' +getCounter + ', sendPost: ' + postCounter)
@@ -43,6 +45,7 @@ var restify = require('restify')
 
     // Get a single product by their products id
 server.get('/products/:id', function (req, res, next) {
+  console.log('>>>' + server.url + '/products:id: recieved GET request')
     
       // Find a single product by their id within save
       productsSave.findOne({ _id: req.params.id }, function (error, product) {
@@ -52,6 +55,7 @@ server.get('/products/:id', function (req, res, next) {
     
         if (product) {
           // Send the product if no issues
+          console.log('<<<' + server.url + '/products:id: sending response')
           res.send(product)
           getCounter++;
           console.log('Processed request count --> sendGet: ' +getCounter + ', sendPost: ' + postCounter)
@@ -64,7 +68,7 @@ server.get('/products/:id', function (req, res, next) {
 
     // Create a new product
 server.post('/products', function (req, res, next) {
-    
+  console.log('>>>' + server.url + '/products: recieved POST request')
       // Make sure name is defined
       if (req.params.name === undefined ) {
         // If there are any errors, pass them to next in the correct format
@@ -86,6 +90,7 @@ server.post('/products', function (req, res, next) {
         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
     
         // Send the product if no issues
+        console.log('<<<' + server.url + '/products: sending response')
         res.send(201, product)
         postCounter++;
         console.log('Processed request count --> sendGet: ' + getCounter + ', sendPost: ' + postCounter)
@@ -94,7 +99,7 @@ server.post('/products', function (req, res, next) {
 
     // Delete product with the given id
 server.del('/products/:id', function (req, res, next) {
-    
+  console.log('>>>' + server.url + '/products:id: recieved DELETE request')
       // Delete the product with the persistence engine
       productsSave.delete(req.params.id, function (error, product) {
     
@@ -102,13 +107,14 @@ server.del('/products/:id', function (req, res, next) {
         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
     
         // Send a 200 OK response
+        console.log('<<<' + server.url + '/products:id: sending response')
         res.send()
       })
     })
 
         // Delete all products
 server.del('/products', function (req, res, next) {
-    
+  console.log('>>>' + server.url + '/products: recieved DELETE request')
       // Delete the product with the persistence engine
       productsSave.deleteMany({},function (error, product) {
     
@@ -116,6 +122,7 @@ server.del('/products', function (req, res, next) {
         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
     
         // Send a 200 OK response
+        console.log('<<<' + server.url + '/products: sending response')
         res.send(204)
       })
     })
